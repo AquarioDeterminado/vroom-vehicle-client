c2af560a56835ab30d44926276425ee2169a3a77
[Aquario Determinado] <aquario.determinado@gmail.com>
2025-03-18 17:44:40 +0000

    Inserting Trips in mongo;

--- src/vehicle_client/vehicle_client.ino
+++ src/vehicle_client/vehicle_client.ino
@@ -1,9 +1,32 @@
+#define S0PIN GPIO32
+#define S1PIN GPIO33
+#define S2PIN GPIO32
+#define S3PIN GPIO25
+#define OUTPIN GPIO26
+#define LEDPIN GPIO27
+
+
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serialbegin(9600);
 
+  //Set UP Wifi connection
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String instruction = Serial.readStringUntil('\n');
+  instruction.trim();
 
+  char* pointer = (char*) instruction.c_str();
+  
+  char* inst = strtok(pointer, " ");
+
+  if (strcmp(inst, "slay") == 0)
+    Serial.write("boots");
 }
